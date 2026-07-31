85387d1484de65b68611424d5bb56c9ba9d56be8
lh <1600778959@qq.com>
2026-07-31 09:38:52 +0800

    refactor: integrate Dima phase 3 RC module chain

--- Dima/product/rover/ApplicationContext.cpp
+++ Dima/product/rover/ApplicationContext.cpp
@@ -1,5 +1,6 @@
 #include "Dima/product/rover/ApplicationContext.hpp"
 
+#include "Dima/middleware/logging/logging.hpp"
 #include "Dima/middleware/uorb/uORB.hpp"
 #include "Dima/middleware/work_queue/WorkQueue.hpp"
 #include "Dima/platform/freertos/dima_platform.hpp"
@@ -21,7 +22,10 @@ ApplicationContext g_application_context;
 
 } // namespace
 
-ApplicationContext::ApplicationContext() noexcept = default;
+ApplicationContext::ApplicationContext() noexcept
+    : sbus_rc_(dima::platform::sbus_uart_backend())
+{
+}
 
 bool ApplicationContext::init() noexcept
 {
@@ -57,6 +61,14 @@ bool ApplicationContext::init() noexcept
         return false;
     }
 #endif
+    if (!module_manager_.register_module(sbus_rc_) ||
+        !module_manager_.register_module(rc_update_) ||
+        !module_manager_.register_module(manual_control_)) {
+        module_manager_.reset();
+        uORB::shutdown();
+        px4::work_queue_shutdown();
+        return false;
+    }
 
     initialized_ = true;
     return true;
@@ -69,9 +81,7 @@ bool ApplicationContext::start() noexcept
     }
 
     boot_started_ = module_manager_.start(boot_health_);
-    if (!boot_started_) {
-        return false;
-    }
+    if (!boot_started_) return false;
 #if APP_HELLO_WORLD_ENABLED
     hello_started_ = module_manager_.start(hello_world_);
     if (!hello_started_) {
@@ -83,34 +93,76 @@ bool ApplicationContext::start() noexcept
     parameter_started_ = parameter_service_.start();
     if (!parameter_started_) {
 #if APP_HELLO_WORLD_ENABLED
-        if (hello_started_) {
-            (void)module_manager_.stop(hello_world_);
-            hello_started_ = false;
-        }
+        (void)module_manager_.stop(hello_world_);
+        hello_started_ = false;
 #endif
         (void)module_manager_.stop(boot_health_);
         boot_started_ = false;
         return false;
     }
+
     log_started_ = log_service_.start();
     if (!log_started_) {
         parameter_service_.stop();
         parameter_started_ = false;
 #if APP_HELLO_WORLD_ENABLED
-        if (hello_started_) {
-            (void)module_manager_.stop(hello_world_);
-            hello_started_ = false;
-        }
+        (void)module_manager_.stop(hello_world_);
+        hello_started_ = false;
 #endif
         (void)module_manager_.stop(boot_health_);
         boot_started_ = false;
         return false;
     }
+
+    // RC 链故障只降级手动输入，不能拖垮参数、日志和恢复服务。
+    if (!start_rc_chain()) {
+        PX4_WARN("Dima RC chain unavailable; actuator output remains disabled");
+    }
     return true;
+}
+
+bool ApplicationContext::start_rc_chain() noexcept
+{
+    sbus_started_ = module_manager_.start(sbus_rc_);
+    if (!sbus_started_) return false;
+
+    rc_update_started_ = module_manager_.start(rc_update_);
+    if (!rc_update_started_) {
+        (void)module_manager_.stop(sbus_rc_);
+        sbus_started_ = false;
+        return false;
+    }
+
+    manual_control_started_ = module_manager_.start(manual_control_);
+    if (!manual_control_started_) {
+        (void)module_manager_.stop(rc_update_);
+        rc_update_started_ = false;
+        (void)module_manager_.stop(sbus_rc_);
+        sbus_started_ = false;
+        return false;
+    }
+    return true;
+}
+
+void ApplicationContext::stop_rc_chain() noexcept
+{
+    if (manual_control_started_) {
+        (void)module_manager_.stop(manual_control_);
+        manual_control_started_ = false;
+    }
+    if (rc_update_started_) {
+        (void)module_manager_.stop(rc_update_);
+        rc_update_started_ = false;
+    }
+    if (sbus_started_) {
+        (void)module_manager_.stop(sbus_rc_);
+        sbus_started_ = false;
+    }
 }
 
 void ApplicationContext::stop() noexcept
 {
+    stop_rc_chain();
     if (log_started_) {
         log_service_.stop();
         log_started_ = false;
--- Dima/product/rover/ApplicationContext.hpp
+++ Dima/product/rover/ApplicationContext.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Dima/modules/boot_health/boot_health.hpp"
+#include "Dima/modules/rc/ManualControl.hpp"
+#include "Dima/modules/rc/RCUpdate.hpp"
+#include "Dima/modules/rc/SbusRc.hpp"
+#include "Dima/platform/freertos/sbus_uart_backend.hpp"
 #include "Dima/middleware/lifecycle/module_manager.hpp"
 #include "Dima/product/rover/LogService.hpp"
 #include "Dima/product/rover/ParameterService.hpp"
@@ -20,10 +24,16 @@ public:
     void stop() noexcept;
 
 private:
+    bool start_rc_chain() noexcept;
+    void stop_rc_chain() noexcept;
+
     dima::middleware::lifecycle::ModuleManager module_manager_{};
     dima::modules::boot_health::BootHealthService boot_health_;
     LogService log_service_{};
     ParameterService parameter_service_{};
+    dima::modules::rc::SbusRc sbus_rc_;
+    dima::modules::rc::RCUpdate rc_update_{};
+    dima::modules::rc::ManualControl manual_control_{};
 #if APP_HELLO_WORLD_ENABLED
     dima::modules::hello_world::HelloWorld hello_world_;
 #endif
@@ -31,6 +41,9 @@ private:
     bool boot_started_{false};
     bool log_started_{false};
     bool parameter_started_{false};
+    bool sbus_started_{false};
+    bool rc_update_started_{false};
+    bool manual_control_started_{false};
 #if APP_HELLO_WORLD_ENABLED
     bool hello_started_{false};
 #endif
